1ab1132bfbc440b196d8632d3924666e81eab3a8
LocoPepe10 <smithivan10@gmail.com>
2019-04-25 23:49:21 -0400

    No funciono

--- somepractice.cpp
+++ somepractice.cpp
@@ -1,10 +1,27 @@
-#include <stdio.h> //directiva include
+#include <stdio.h>
 
+int suma();
 int main()
 {
+	int num1,num2;
+	printf("Ingrese primer valor\n");
+	scanf("%i",&num2);
 	
+	printf("Ingrese segundo valor\n");
+	scanf("%i",&num1);
+	
+	printf("%i\n", suma(num1, num2));
 	return 0;
-
 }
+
+int suma(int num1, int num2)
+{
+	int suma = num1 + num2;
+	return suma;
+}
+
+
+
+
 
 
